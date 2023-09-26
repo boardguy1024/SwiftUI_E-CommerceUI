e2a84c6358915da5e23742fb13957aa0bd3d6dd8
パクギョンソク <boardguy1024@gmail.com>
2023-09-26 22:57:03 +0900

    Initial Commit

---
+++ SwiftUI_E-CommerceUI/SwiftUI_E_CommerceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_E_CommerceUIApp.swift
+//  SwiftUI_E-CommerceUI
+//
+//  Created by パクギョンソク on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_E_CommerceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
